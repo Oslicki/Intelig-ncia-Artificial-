add12ebd9950c69eab8d28725634abc86f363afa
Oslicki <163071062+Oslicki@users.noreply.github.com>
2024-09-02 20:06:22 -0300

    top

--- main.js
+++ main.js
@@ -14,59 +14,59 @@ const perguntas = [
             },
             {
                 texto: " A água não é importante para a vida na Terra; a maioria dos organismos pode viver sem ela.",
-                afirmacao: "incorreto."
+                afirmacao: "incorreto!"
             }
         ]
     },
     {
-        enunciado: "Com a descoberta desta tecnologia, chamada Inteligência Artificial, uma professora de tecnologia da escola decidiu fazer uma sequência de aulas sobre esta tecnologia. No fim de uma aula ela pede que você escreva um trabalho sobre o uso de IA em sala de aula. Qual atitude você toma?",
+        enunciado: "Como a água é distribuída no planeta?",
         alternativas: [
             {
-                texto: "Utiliza uma ferramenta de busca na internet que utiliza IA para que ela ajude a encontrar informações relevantes para o trabalho e explique numa linguagem que facilite o entendimento.",
-                afirmacao: "Conseguiu utilizar a IA para buscar informações úteis."
+                texto: "A água está distribuída principalmente em oceanos, com pequenas quantidades em lagos, rios, geleiras e águas subterrâneas.",
+                afirmacao: "Correto!"
             },
             {
-                texto: "Escreve o trabalho com base nas conversas que teve com colegas, algumas pesquisas na internet e conhecimentos próprios sobre o tema.",
-                afirmacao: "Sentiu mais facilidade em utilizar seus próprios recursos para escrever seu trabalho."
+                texto: "A água está distribuída igualmente em todos os lugares do planeta e é encontrada principalmente em desertos e montanhas.",
+                afirmacao: "Incorreto!"
             }
         ]
     },
     {
-        enunciado: "Após a elaboração do trabalho escrito, a professora realizou um debate entre a turma para entender como foi realizada a pesquisa e escrita. Nessa conversa também foi levantado um ponto muito importante: como a IA impacta o trabalho do futuro. Nesse debate, como você se posiciona?",
+        enunciado: "Como as mudanças climáticas estão impactando os recursos hídricos?",
         alternativas: [
             {
-                texto: "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
-                afirmacao: "Vem impulsionando a inovação na área de IA e luta para abrir novos caminhos profissionais com IA."
+                texto: "As mudanças climáticas não têm impacto significativo sobre os recursos hídricos e apenas afetam o clima.",
+                afirmacao: "Incorreto!"
             },
             {
-                texto: "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores.",
-                afirmacao: "Sua preocupação com as pessoas motivou a criar um grupo de estudos entre trabalhadores para discutir meios de utilização de IA de forma ética."
+                texto: "As mudanças climáticas podem causar alterações nos padrões de precipitação, aumentar a frequência de eventos extremos e afetar a disponibilidade de água em várias regiões.",
+                afirmacao: "Correto!"
             }
         ]
     },
     {
-        enunciado: "Ao final da discussão, você precisou criar uma imagem no computador que representasse o que pensa sobre IA. E agora?",
+        enunciado: "Como a tecnologia de dessalinização funciona e quais são suas vantagens e desvantagens?",
         alternativas: [
             {
-                texto: "Criar uma imagem utilizando uma plataforma de design como o Paint.",
-                afirmacao: "Notou também que muitas pessoas não sabem ainda utilizar as ferramentas tradicionais e decidiu compartilhar seus conhecimentos de design utilizando ferramentas de pintura digital para iniciantes."
+                texto: " A dessalinização remove o sal da água do mar através de processos como a osmose reversa. Vantagens incluem a criação de água potável a partir de água do mar, e desvantagens incluem o alto custo e impacto ambiental dos resíduos.",
+                afirmacao: "Correto!"
             },
             {
-                texto: "Criar uma imagem utilizando um gerador de imagem de IA.",
-                afirmacao: "Acelerou o processo de criação de trabalhos utilizando geradores de imagem e agora consegue ensinar pessoas que sentem dificuldades em desenhar manualmente como utilizar também!"
+                texto: "Dessalinização adiciona sal à água doce para torná-la mais saborosa e acessível. É uma tecnologia barata e não tem desvantagens.",
+                afirmacao: "Incorreto!"
             }
         ]
     },
     {
-        enunciado: "Você tem um trabalho em grupo de biologia para entregar na semana seguinte, o andamento do trabalho está um pouco atrasado e uma pessoa do seu grupo decidiu fazer com ajuda da IA. O problema é que o trabalho está totalmente igual ao do chat. O que você faz? ",
+        enunciado: "Qual é o impacto das atividades agrícolas sobre os recursos hídricos?",
         alternativas: [
             {
-                texto: "Escrever comandos para o chat é uma forma de contribuir com o trabalho, por isso não é um problema utilizar o texto inteiro.",
-                afirmacao: "Infelizmente passou a utilizar a IA para fazer todas suas tarefas e agora se sente dependente da IA para tudo."
+                texto: "Atividades agrícolas podem causar poluição de águas superficiais e subterrâneas por meio de escoamento de fertilizantes e pesticidas, além de exaurir os recursos hídricos devido ao uso intensivo de água para irrigação, afetando a sustentabilidade e a qualidade dos recursos hídricos.",
+                afirmacao: "Correto!"
             },
             {
-                texto: "O chat pode ser uma tecnologia muito avançada, mas é preciso manter a atenção pois toda máquina erra, por isso revisar o trabalho e contribuir com as perspectivas pessoais é essencial.",
-                afirmacao: "Percebeu que toda IA reproduz orientações baseadas na empresa que programou e muito do que o chat escrevia não refletia o que pensava e por isso sabe que os textos gerados pela IA devem servir como auxílio e não resultado final. "
+                texto: "As atividades agrícolas geralmente melhoram a qualidade dos recursos hídricos e não têm efeitos negativos significativos sobre a disponibilidade ou a pureza da água.",
+                afirmacao: "Incorreto!"
             }
         ]
     },
@@ -105,7 +105,7 @@ function respostaSelecionada(opcaoSelecionada) {
 }
 
 function mostraResultado() {
-    caixaPerguntas.textContent = "Em 2049...";
+    caixaPerguntas.textContent = "RESPOSTAS";
     textoResultado.textContent = historiaFinal;
     caixaAlternativas.textContent = "";
 }
